a19a8473f9de5c7d02d219b8603f50b30eac4420
Lex <100075440+lexkh@users.noreply.github.com>
2023-10-17 01:47:55 +0300

    https://github.com/lexkh/first-project-fashion-react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './components/header/header.jsx';
 import Promo from './components/promo/promo.jsx';
 import Brands from './components/brands/brands.jsx';
 import Arrivals from './components/arrivals/arrivals.jsx';
+import Voucher from './components/voucher/voucher.jsx'
+import Download from './components/download/download.jsx'
 function App() {
   return (
 		<div className="App">
@@ -9,6 +11,8 @@ function App() {
       <Promo/>
       <Brands/>
       <Arrivals/>
+      <Voucher/>
+      <Download/>
 		</div>
   );
 }
